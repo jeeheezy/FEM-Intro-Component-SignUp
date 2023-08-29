2d63874bdb26f8edf6b3812c1b7a857a87016551
Jeeho Lee <jeeho032399@gmail.com>
2023-08-29 17:07:15 -0500

    Few changes on HTML to add classes, starting javascript (idk what I'm doing)

---
+++ index.js
@@ -0,0 +1,12 @@
+const claim = document.querySelector('.submit');
+
+function checkEmpty() {
+  let inputs = document.querySelectorAll('input[type=text]');
+  let emptyflags;
+  emptyflags= inputs.forEach(input => w)
+  if () {
+
+  }
+}
+
+claim.addEventListener('submit', checkEmpty);
