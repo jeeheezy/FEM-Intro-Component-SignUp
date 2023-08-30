c3e1f2c74b76e9ca74e6a7b217c847116f11128a
Jeeho Lee <jeeho032399@gmail.com>
2023-08-30 15:45:20 -0500

    Updates to CSS and JS for simplistic email validation

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const form = document.querySelector("form");
+const email = document.querySelector("input[name=email]")
 
-function checkEmpty(e) {
-  e.preventDefault(); // prevent reload
-  // remove error message and margin changes from previous submissions
-  document.querySelectorAll('.fieldemptyerror').forEach(e => e.remove()); 
+function checkEmptyField() {
   const inputsNodeList = document.querySelectorAll('input[type=text]');
+  // remove margin changes from previous submission attempts
   inputsNodeList.forEach(input => input.classList.remove("changedform"));
 
   // querySelectorAll returns NodeList, convert to array to use .map
@@ -13,21 +12,47 @@ function checkEmpty(e) {
 
   // if no value in field, present error message
   inputLength.forEach((length, index) => {
-    if(length == 0) {
-      addEmptyFieldError(inputsNodeList[index]);
+    if(length === 0) {
+      let errorPhrase = inputsNodeList[index].placeholder + " cannot be empty";
+      addError(inputsNodeList[index], errorPhrase);
     }
   })
 }
 
-function addEmptyFieldError(inputToChange) {
+function checkEmailAddress() {
+  /* simplistic email validation, see complexity of regex validation in this article:
+  https://stackoverflow.com/questions/46155/how-can-i-validate-an-email-address-in-javascript?page=1&tab=scoredesc#tab-top*/
+  if (email.value.length === 0) {
+    return;
+  }
+  else {
+    const pattern = /^\S+@\S+\.\S+$/;
+    let result = pattern.test(email.value);
+    if (result === false) {
+      addError(email, "Looks like this is not an email");
+      email.classList.add("incorrectemail");
+    }
+  }
+}
+
+function addError(inputToChange, errorMessage) {
   // create new paragraph element with class of error and populate with error message
   const newParagraph = document.createElement("p");
-  newParagraph.classList.add("fieldemptyerror");
-  newParagraph.innerHTML = inputToChange.placeholder + " cannot be empty";
-
+  newParagraph.classList.add("error");
+  newParagraph.innerHTML = errorMessage
   // add error message, add class
   inputToChange.insertAdjacentElement('afterend', newParagraph);
   inputToChange.classList.add("changedform")
 }
 
-form.addEventListener('submit', checkEmpty);
+function formValidation(e) {
+  e.preventDefault(); // prevent reload
+  // remove error message from previous submission
+  document.querySelectorAll('.error').forEach(e => e.remove());
+  checkEmptyField();
+  checkEmailAddress()
+}
+
+form.addEventListener('submit', formValidation);
+// remove styles added to text from incorrect email, but want to remove red color on click instead of on submission
+email.addEventListener('click', () => email.classList.remove("incorrectemail"))
