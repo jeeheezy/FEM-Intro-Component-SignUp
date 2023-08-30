c23101b4849b7dbac6ba07299abb73ec233941e7
Jeeho Lee <jeeho032399@gmail.com>
2023-08-30 14:47:31 -0500

    Adjusted HTML and CSS to present classes to work alongside JS, updated JS to validate text field isn't empty on submission

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
-const claim = document.querySelector('.submit');
+const form = document.querySelector("form");
 
-function checkEmpty() {
-  let inputs = document.querySelectorAll('input[type=text]');
-  let emptyflags;
-  emptyflags= inputs.forEach(input => w)
-  if () {
+function checkEmpty(e) {
+  e.preventDefault(); // prevent reload
+  // remove error message and margin changes from previous submissions
+  document.querySelectorAll('.fieldemptyerror').forEach(e => e.remove()); 
+  const inputsNodeList = document.querySelectorAll('input[type=text]');
+  inputsNodeList.forEach(input => input.classList.remove("changedform"));
 
-  }
+  // querySelectorAll returns NodeList, convert to array to use .map
+  const inputsArray = Array.from(inputsNodeList);
+  const inputLength = inputsArray.map(input => input.value.length);
+
+  // if no value in field, present error message
+  inputLength.forEach((length, index) => {
+    if(length == 0) {
+      addEmptyFieldError(inputsNodeList[index]);
+    }
+  })
 }
 
-claim.addEventListener('submit', checkEmpty);
+function addEmptyFieldError(inputToChange) {
+  // create new paragraph element with class of error and populate with error message
+  const newParagraph = document.createElement("p");
+  newParagraph.classList.add("fieldemptyerror");
+  newParagraph.innerHTML = inputToChange.placeholder + " cannot be empty";
+
+  // add error message, add class
+  inputToChange.insertAdjacentElement('afterend', newParagraph);
+  inputToChange.classList.add("changedform")
+}
+
+form.addEventListener('submit', checkEmpty);
